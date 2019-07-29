4ec8e4b47859b211e462175cf503c20aaca61e73
Sevastyan Basharin <sv.acid6@gmail.com>
2019-07-30 00:08:06 +0400

    new repo

---
+++ main.py
@@ -0,0 +1,14 @@
+import telebot
+import os
+
+
+token = os.environ.get('TOKEN', None)
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_command(message):
+    bot.send_message(message.chat.id, 'привет! Вот токен' + token)
+
+
+bot.polling(True)
