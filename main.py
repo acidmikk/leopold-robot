806909b7b3ce11b40f67f9c7ce346c0c86abf2c5
Sevastyan Basharin <sv.acid6@gmail.com>
2019-07-30 23:51:53 +0400

    fix action

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def handle_command(message):
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    bot.send_chat_action(message.chat.id, 'typing')
     if message.text.lower() == 'что делаешь, лео?':
+        bot.send_chat_action(message.chat.id, 'typing')
         time.sleep(2)
         bot.send_message(message.chat.id, 'Привет! Я учусь читать и писать', reply_to_message_id=message.message_id)
 
