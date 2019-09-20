dfc07eb483ff00bedce877c77fde02ec3a8753ee
Sevastyan Basharin <sv.acid6@gmail.com>
2019-09-20 20:19:53 +0400

    первые сообщения

--- functions.py
+++ functions.py
@@ -1,4 +1,14 @@
-def read_text(text):
-    for i in text:
-        if i in dictionary:
-            bot.send_message(message.chat.id, answer)
+import requests, os
+
+word_and_synonims = []
+words_discription = []
+
+
+def update():
+    url = os.environ.get('URL', None)
+    response = requests.get(url).json()
+    word_list = response['data']
+    for word in word_list:
+        word_and_synonims.append([word['attributes']['main'],
+                                  *word['attributes']['synonims'][:]])
+        words_discription.append(word['attributes']['description'])
--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import telebot
-import os
 import functions
-import time
+import time, os
 
 
 token = os.environ.get('TOKEN', None)
 bot = telebot.TeleBot(token)
 
+def typing_action(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    time.sleep(2)
 
 @bot.message_handler(commands=['start'])
 def handle_command(message):
+    typing_action(message)
     bot.send_message(message.chat.id, 'Привет! Меня зовут Леопольд и я помогу '
                                       'разобраться тебя в терминологии IT')
 
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+    message = message.split()
+    words = functions.word_and_synonims
+    discriptions = functions.words_discription
+    for word in message:
+        for i in len(words):
+            if word in words[i]:
+                answer = word + ' или же ' + words[i][0] + ' - это ' + discriptions[i]
+                bot.send_message(message.chat.id, answer)
     if message.text.lower() == 'что делаешь, лео?':
-        bot.send_chat_action(message.chat.id, 'typing')
-        time.sleep(2)
+        typing_action(message)
         bot.send_message(message.chat.id, 'Привет! Я учусь читать и писать', reply_to_message_id=message.message_id)
 
 
